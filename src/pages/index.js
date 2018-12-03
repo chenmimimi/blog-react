4573390e911ea68890a4eeebec4a3f0a7978b8fa
chenmi <soulme.chen@outlook.com>
2018-12-03 17:22:46 +0800

    mail

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Index extends Component {
   render() {
     return (
-      <div>测试mail配置</div>
+      <div>测试mail</div>
     );
   }
 }
